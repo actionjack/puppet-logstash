16a35bca49f9b0a99eca312fabc4c9c9b60c19a1
Martin Jackson <mjackson@equalexperts.com>
2012-10-22 10:06:31 +0000

    Whitespace client up

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,7 +20,7 @@ class logstash::params {
   if $amqp_exchange_name == undef {
     $amqp_exchange_name = hiera('amqp_exchange_name'),
   }
-  if $amqp_virtual_host == undef { 
+  if $amqp_virtual_host == undef {
     $amqp_virtual_host  = hiera('amqp_virtual_host'),
   }
   if $amqp_username == undef {
