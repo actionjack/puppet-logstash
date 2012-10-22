7f80c9327388f5cd3aefccfe7da092d6e0c315d3
Martin Jackson <mjackson@equalexperts.com>
2012-10-22 12:27:32 +0000

    Fixed syntax error

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,24 +12,24 @@
 #
 class logstash::params {
   if $amqp_host == undef {
-    $amqp_host          = hiera('amqp_host'),
+    $amqp_host          = hiera('amqp_host')
   }
   if $amqp_queue_name == undef {
-    $amqp_queue_name    = hiera('amqp_queue_name'),
+    $amqp_queue_name    = hiera('amqp_queue_name')
   }
   if $amqp_exchange_name == undef {
-    $amqp_exchange_name = hiera('amqp_exchange_name'),
+    $amqp_exchange_name = hiera('amqp_exchange_name')
   }
   if $amqp_virtual_host == undef {
-    $amqp_virtual_host  = hiera('amqp_virtual_host'),
+    $amqp_virtual_host  = hiera('amqp_virtual_host')
   }
   if $amqp_username == undef {
-    $amqp_username      = hiera('amqp_username'),
+    $amqp_username      = hiera('amqp_username')
   }
   if $amqp_userpassword == undef {
-    $amqp_userpassword  = hiera('amqp_userpassword'),
+    $amqp_userpassword  = hiera('amqp_userpassword')
   }
   if $elasticsearch_host == undef {
-    $elasticsearch_host = hiera('elasticsearch_host'),
+    $elasticsearch_host = hiera('elasticsearch_host')
   }
 }
